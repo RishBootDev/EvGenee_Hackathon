959779f27eee56ac68cad3d45c27e6805a61a71a
Ritul Jain <rituljain202@gmail.com>
2026-05-02 13:06:19 +0530

    setting nodemailer

--- EvGenee_Backend/src/config/config.js
+++ EvGenee_Backend/src/config/config.js
@@ -6,7 +6,7 @@ const JWT_KEY=process.env.JWT_KEY;
 const RAZORPAY_KEY_ID=process.env.RAZORPAY_KEY_ID;
 const RAZORPAY_KEY_SECRET=process.env.RAZORPAY_KEY_SECRET;
 
-if(!PORT || !MONGO_URI ||!JWT_KEY || !RAZORPAY_KEY_ID || !RAZORPAY_KEY_SECRET){
+if(!PORT || !MONGO_URI ||!JWT_KEY || !RAZORPAY_KEY_ID || !RAZORPAY_KEY_SECRET ){
     throw new Error ("Environment Variable not provided");
 }
 
@@ -16,5 +16,5 @@ module.exports={
     MONGO_URI,
     JWT_KEY,
     RAZORPAY_KEY_ID,
-    RAZORPAY_KEY_SECRET
+    RAZORPAY_KEY_SECRET,
 }
